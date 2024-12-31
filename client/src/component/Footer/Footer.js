c7115ad1ad8e4e8a1cd3ca8c20c47af07e2e6251
Bhargav S <157606103+BhargavvS@users.noreply.github.com>
2024-12-31 14:14:54 +0530

    Update Footer.js

--- client/src/component/Footer/Footer.js
+++ client/src/component/Footer/Footer.js
@@ -7,31 +7,7 @@ const Footer = () => (
     <div className="footer-block"></div>
     <div className="footer">
       <div className="footer-container">
-        {/* <p>
-          View this project on{" "}
-          <a
-            className="profile"
-            href="https://github.com/arlbibek/dVoting"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          .
-        </p> */}
-        {/* <p>
-          Made with <i className="fas fa-heartbeat" /> by{" "}
-          <a
-            className="profile"
-            href="https://arlbibek.github.io"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Decentralized voting System  
-          </a>
-          .
-        </p> */}
-
+        {
         <p className="name">
         Decentralized voting System
         </p>
